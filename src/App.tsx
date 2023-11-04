6c42e1d83bfacec7bdfa0f4bfa20b4355d7b8941
M7kra <luiswbarbosa@gmail.com>
2023-11-04 13:02:37 +0000

    Fixed problem with slow css loading

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,20 @@
 import FlowField from './components/FlowField/flowfield';
 import { Config } from './components/FlowField/types';
-import '@m7kra/react-utils/lib/css/theme.css';
 import { Github } from 'react-bootstrap-icons';
 import { useState } from 'react';
-
+import getColors from './utils';
+import '@m7kra/react-utils/lib/css/theme.css';
 import './App.css';
 
 export default function App() {
     const [config, setConfig] = useState<Config>(getConfig());
     window.onresize = () => setConfig(getConfig());
 
-    const surfaceColor = getComputedStyle(document.documentElement).getPropertyValue('--surface');
-    const cardColor = transparentColor(surfaceColor, 0.9);
+    const colors = getColors();
 
     return <div id='app' className='surface'>
         <FlowField config={config} />
-        <div id='card' style={{backgroundColor: cardColor}}>
+        <div id='card' style={{backgroundColor: colors.backgroundTransparent}}>
             <h1>FlowFields</h1>
             <p><a className='primary-link' href='https://m7kra.github.io'>M7kra</a>'s hidden talent for art (and plagiarism)</p>
             <a className='on-surface-link' href='https://github.com/flow'><Github size={32} /></a>
@@ -28,9 +27,11 @@ function getConfig() {
     const pointCount = Math.round((window.innerWidth * window.innerHeight) / 1500);
     // The same ratio works for the scale
     const scale = Math.round((window.innerWidth * window.innerHeight) / 1500);
+
+    const colors = getColors();
     return {
-        color: transparentColor(getComputedStyle(document.documentElement).getPropertyValue('--on-surface'), 0.1),
-        backgroundColor: getComputedStyle(document.documentElement).getPropertyValue('--surface'),
+        color: colors.color,
+        backgroundColor: colors.background,
         pointCount,
         width: window.innerWidth,
         height: window.innerHeight,
@@ -39,9 +40,4 @@ function getConfig() {
         pointSpeed: 100,
         pointMass: 0.04
     };
-}
-
-function transparentColor(color: string, alpha: number) {
-    const [r, g, b] = color.match(/\d+/g)!.map(Number);
-    return `rgba(${r}, ${g}, ${b}, ${alpha})`;
 }
